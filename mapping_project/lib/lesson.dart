3e293a5b6055ab1e4f3cd91475e0c6baac84c0ba
ColtonZ <55670585+ColtonZ@users.noreply.github.com>
2020-06-08 14:04:33 +0100

    Added comments

--- mapping_project/lib/lesson.dart
+++ mapping_project/lib/lesson.dart
@@ -1,11 +1,15 @@
+//this is a class for each lesson
 class Lesson {
+  //this just says which variables are needed for each lesson object
   final String subject;
   final String room;
   final String teacher;
   final String time;
 
+  //constructor
   Lesson({this.subject, this.room, this.teacher, this.time});
 
+  //creates a lesson object, given a line from a text file
   factory Lesson.fromString(String string) {
     var details = string.split(', ');
     return Lesson(
@@ -16,6 +20,7 @@ class Lesson {
     );
   }
 
+  //outputs the lesson's details
   void output() {
     print("subject: $subject | room: $room | teacher: $teacher | time: $time");
   }
