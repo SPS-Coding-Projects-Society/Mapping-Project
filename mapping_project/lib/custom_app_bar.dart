85c6ef04f13c5d78b25ba49dbc766b3a592f5518
Zack Colton <55670585+ColtonZ@users.noreply.github.com>
2020-06-23 10:59:00 +0100

    Merged all pages - still slightly buggy!

--- mapping_project/lib/custom_app_bar.dart
+++ mapping_project/lib/custom_app_bar.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'settings_page.dart';
 import 'lessons_page.dart';
+import 'directionsSummaryPage.dart';
+import 'myhomepage.dart';
+import 'tannoy.dart';
 
 class CustomAppBar {
   static void _pushSettingsPage(BuildContext context) {
@@ -12,6 +15,24 @@ class CustomAppBar {
   static void _pushLessonsPage(BuildContext context) {
     Navigator.of(context).push(
       MaterialPageRoute(builder: (BuildContext context) => LessonsPage()),
+    );
+  }
+
+  static void _pushDirectionsPage(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (BuildContext context) => SummaryView()),
+    );
+  }
+
+  static void _pushHomePage(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (BuildContext context) => MyHomePage()),
+    );
+  }
+
+  static void _pushTannoyPage(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (BuildContext context) => Tannoy()),
     );
   }
 
@@ -29,17 +50,38 @@ class CustomAppBar {
               case "settings":
                 _pushSettingsPage(context);
                 break;
+              case "directions":
+                _pushDirectionsPage(context);
+                break;
+              case "home":
+                _pushHomePage(context);
+                break;
+              case "tannoy":
+                _pushTannoyPage(context);
+                break;
               default:
             }
           },
           itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
             const PopupMenuItem<String>(
-              value: "lesson",
-              child: Text("Lessons Page"),
+              value: "home",
+              child: Text("Home Page"),
+            ),
+            const PopupMenuItem<String>(
+              value: "directions",
+              child: Text("Directions Page"),
             ),
             const PopupMenuItem<String>(
               value: "settings",
               child: Text("Settings Page"),
+            ),
+            const PopupMenuItem<String>(
+              value: "lessons",
+              child: Text("Lessons Page"),
+            ),
+            const PopupMenuItem<String>(
+              value: "tannoy",
+              child: Text("Tannoy Page"),
             ),
           ],
         )
