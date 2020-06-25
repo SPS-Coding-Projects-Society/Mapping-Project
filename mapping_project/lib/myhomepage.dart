bc2536b108c389676f542cafbaca6d3e85920b51
Zack Colton <55670585+ColtonZ@users.noreply.github.com>
2020-06-25 17:38:33 +0100

    Converted jfifs to jpgs

--- mapping_project/lib/myhomepage.dart
+++ mapping_project/lib/myhomepage.dart
@@ -15,6 +15,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
   var maps = [
+    'assets/images/Whole School 1.jfif',
     'assets/images/SPSMAP_Top.svg',
     'assets/images/SPSMAP_3rd.svg',
     'assets/images/SPSMAP_2nd.svg',
